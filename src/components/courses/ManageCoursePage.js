8b831c2e15fd045e7784ad1864f69fbe03ae4df9
David Elì <davidelimg1@gmail.com>
2020-09-09 23:50:21 -0500

    Aded updated action

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 import CourseForm from './CourseForm';
 import { newCourse } from '../../../tools/mockData';
 
-const ManageCoursePage = ({
+function ManageCoursePage({
   courses,
   authors,
   loadAuthors,
@@ -14,7 +14,7 @@ const ManageCoursePage = ({
   saveCourse,
   history,
   ...props
-}) => {
+}) {
   const [course, setCourse] = useState({ ...props.course });
   const [errors, setErrors] = useState({});
 
@@ -23,6 +23,8 @@ const ManageCoursePage = ({
       loadCourses().catch(error => {
         alert('Loading courses failed' + error);
       });
+    } else {
+      setCourse({ ...props.course });
     }
 
     if (authors.length === 0) {
@@ -30,22 +32,22 @@ const ManageCoursePage = ({
         alert('Loading authors failed' + error);
       });
     }
-  }, []);
+  }, [props.course]);
 
-  const handleChange = event => {
+  function handleChange(event) {
     const { name, value } = event.target;
     setCourse(prevCourse => ({
       ...prevCourse,
       [name]: name === 'authorId' ? parseInt(value, 10) : value,
     }));
-  };
+  }
 
-  const handleSave = event => {
+  function handleSave(event) {
     event.preventDefault();
     saveCourse(course).then(() => {
       history.push('/courses');
     });
-  };
+  }
 
   return (
     <CourseForm
@@ -56,25 +58,34 @@ const ManageCoursePage = ({
       onSave={handleSave}
     />
   );
-};
+}
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
-  courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
   saveCourse: PropTypes.func.isRequired,
   history: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => {
+export function getCourseBySlug(courses, slug) {
+  return courses.find(course => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors,
   };
-};
+}
 
 const mapDispatchToProps = {
   loadCourses,
