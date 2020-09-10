562a04a72ad092044a19203d50e69ae4d7ac44ec
David Elì <davidelimg1@gmail.com>
2020-09-10 00:10:36 -0500

    Fixed issue on courseReducer

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -6,9 +6,9 @@ const courseReducer = (state = initialState.courses, action) => {
     case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
     case types.UPDATE_COURSE_SUCCESS:
-      return state.map(course => {
-        course.id === action.course.id ? action.course : course;
-      });
+      return state.map(course =>
+        course.id === action.course.id ? action.course : course,
+      );
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
     default:
